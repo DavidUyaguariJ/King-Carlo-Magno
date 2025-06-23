c89e6dc881306549ab16f2d5430e41287924179e
DavidUyaguariJ <david.uyaguari@udla.edu.ec>
2025-06-23 11:24:34 -0500

    feat: add serial comunication to player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     float speed = 1f;
-	LayerMask mask;
-	public float distance = 5f;
-	public Texture2D pointer;
-	public GameObject textDetect;
-	GameObject lastRecognized = null;
+    LayerMask mask;
+    public float distance = 5f;
+    public Texture2D pointer;
+    public GameObject textDetect;
+    GameObject lastRecognized = null;
 
+    public SerialReader serialReader;
+    public float threshold = 0.05f;
 
     public void makeWalk()
     {
-        float verticalInput = Input.GetAxis("Vertical");
-		float horizontalInput = Input.GetAxis("Horizontal");
-		Vector3 movementDirection = new Vector3(horizontalInput,0, verticalInput);
+        Vector3 movementDirection = Vector3.zero;
+
+        if (serialReader != null)
+        {
+            Vector3 accel = serialReader.accel;
+            if (accel.x > threshold)
+                movementDirection.x = 1f;
+            else if (accel.x < -threshold)
+                movementDirection.x = -1f;
+            if (accel.y > threshold)
+                movementDirection.z = 1f;
+            else if (accel.y < -threshold)
+                movementDirection.z = -1f;
+        }
+
         movementDirection.Normalize();
-        transform.position = transform.position + movementDirection * speed * Time.deltaTime;
+        transform.position += movementDirection * speed * Time.deltaTime;
     }
 
     void Start()
@@ -27,60 +41,53 @@ public class Player : MonoBehaviour
         textDetect.SetActive(false);
     }
 
-
-    private void Update()
-	{
-		RaycastHit hit;
-		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, mask))
-		{
-			DeselectObject();
-			SelectedObject(hit.transform);
-		}
-		else {
-			DeselectObject();
-		}
-	}
-
-	public bool interactWithObject()
+    void Update()
     {
-		RaycastHit hit;
-		Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, mask);
-		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, mask))
-		{
-			if (hit.collider.tag == "bounty")
-			{
-				return hit.collider.transform.GetComponent<AddOn>().activeAddOn();
-			}
-		}
-			return false;
-	}
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, mask))
+        {
+            DeselectObject();
+            SelectedObject(hit.transform);
+        }
+        else
+        {
+            DeselectObject();
+        }
+    }
 
-	void SelectedObject(Transform transform) 
-	{
-		transform.GetComponent<MeshRenderer>().material.color = Color.green;
-		lastRecognized = transform.gameObject;
-	}
-	void DeselectObject() 
-	{
-		if (lastRecognized) 
-		{ 
-			lastRecognized.GetComponent<Renderer>().material.color = Color.white;
-			lastRecognized = null;
-		}
-	}
+    public bool interactWithObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, mask))
+        {
+            if (hit.collider.tag == "bounty")
+            {
+                return hit.collider.transform.GetComponent<AddOn>().activeAddOn();
+            }
+        }
+        return false;
+    }
 
-	void OnGUI()
-	{
-		Rect rect = new Rect(Screen.width, Screen.height, pointer.width, pointer.height);
-		GUI.DrawTexture(rect, pointer);
+    void SelectedObject(Transform transform)
+    {
+        transform.GetComponent<MeshRenderer>().material.color = Color.green;
+        lastRecognized = transform.gameObject;
+    }
 
-		if (lastRecognized)
-		{
-			textDetect.gameObject.SetActive(true);
-		}
-		else
-		{
-			textDetect.gameObject.SetActive(false);
-		}
-	}
+    void DeselectObject()
+    {
+        if (lastRecognized)
+        {
+            lastRecognized.GetComponent<Renderer>().material.color = Color.white;
+            lastRecognized = null;
+        }
+    }
+
+    void OnGUI()
+    {
+        Rect rect = new Rect(Screen.width, Screen.height, pointer.width, pointer.height);
+        GUI.DrawTexture(rect, pointer);
+
+        textDetect.SetActive(lastRecognized != null);
+    }
 }
